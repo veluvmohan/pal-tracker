feeb3b0dd2bd1761c47691d8dd101eac2c7f434e
pal <pal>
2018-12-04 14:40:35 +0530

    Retoched the file for travis to work.

--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WelcomeControllerTest {
-
     @Test
     public void itSaysHello() throws Exception {
         WelcomeController controller = new WelcomeController("A welcome message");
